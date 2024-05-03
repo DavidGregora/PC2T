b3110b28470524719b95125a76c26429361cef44
KatarinaKudelova <145453877+KatarinaKudelova@users.noreply.github.com>
2024-05-03 14:03:20 +0200

    Update ucebnica.java

--- ucebnica.java
+++ ucebnica.java
@@ -6,7 +6,7 @@ public class ucebnica extends kniha{
 	private int rocnik;
 	
 	public ucebnica(String nazov, List<String> autori, int rokVydania, int rocnik) {
-		super(nazov, autori, rokVydania, nazov);
+		super(nazov, autori, rokVydania, ZanerRomanu.ROMANTICKY);
 		this.rocnik = rocnik;
 	}
 	
